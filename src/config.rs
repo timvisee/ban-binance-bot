5d595cd113f5f547af3c0c31ab949d836cc1fb58
timvisee <tim@visee.me>
2019-10-04 00:48:17 +0200

    Increase attachment size limit

--- src/config.rs
+++ src/config.rs
@@ -26,7 +26,7 @@ pub const ILLEGAL_IMAGE_TEXT: [&str; 4] = [
 pub const ILLEGAL_IMAGES_DIR: &str = "./res/illegal/";
 
 /// The maximum file size in bytes of files to check for legality.
-pub const MAX_FILE_SIZE: i64 = 2 * 1024 * 1024;
+pub const MAX_FILE_SIZE: i64 = 10 * 1024 * 1024;
 
 /// Images are illegal when their similarity to any template image is `<= threhold`.
 pub const IMAGE_BAN_THRESHOLD: f64 = 0.5;
