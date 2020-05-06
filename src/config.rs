cb4fda7ee116528f9f37a1365d24f52917493512
timvisee <tim@visee.me>
2020-05-06 17:12:19 +0200

    Remove redundant hard coded configuration values

--- src/config.rs
+++ src/config.rs
@@ -55,68 +55,14 @@ pub struct Image {
     pub text: Vec<String>,
 }
 
-/// A list of illegal URL host parts.
-pub const ILLEGAL_HOST_PARTS: [&str; 13] = [
-    "binance.bnb",
-    "binance.dex",
-    "binance.event",
-    "binance.exchange",
-    "binance.jersey",
-    "binance.market",
-    "exchange.2019e",
-    "exchange.bnb",
-    "exchange.channelevent",
-    "exchange.dexmx",
-    "exchange.projectdex",
-    "jerseyonline",
-    "jerseysolution",
-];
-
-/// A list of illegal text.
-pub const ILLEGAL_TEXT: [&str; 5] = [
-    "Celebrating Our New Crypto Exchange",
-    "Binance is pleased to announce the unmatched trading",
-    "To celebrate the launch of Binance US",
-    // "Event ends today!",
-    "First 5000 Participants Bonus",
-    "Only the first 5000 users will be rewarded",
-];
-
-/// A list of illegal text in webpage bodies.
-pub const ILLEGAL_WEBPAGE_TEXT: [&str; 9] = [
-    "First 5000 Participants BTC Giveaway!",
-    "Celebrating the launch of our new Crypto Marketplace - Binance US",
-    "We are pleased to announce the unmatched trading technology platform of Binance to the United States and all of North America",
-    "To celebrate the launch of Binance US, we are rewarding the first 5000 participants with 10 times deposit bonus",
-    "In order to be eligible, participants must have a minimum of 0.02 BTC",
-    "Only the first 5000 users will be rewarded and it's on a first come first served basis. Qualifying users will receive the deposit bonus along with an invitation link to beta test Binance US. Every bug/hack/problem found on Binance US will be rewarded up to 10 BTC (more details upon sign-up).",
-    "For every BTC contributed, you will receive back 10 times more BTC!",
-    r#"<html><body><script type="text/javascript" src="/aes.js" ></script><script>function toNumbers(d){var e=[];d.replace(/(..)/g"#,
-    r#"&i=1";</script><noscript>This site requires Javascript to work, please enable Javascript in your browser or use a browser with Javascript support</noscript></body></html>"#,
-];
-
 /// A list of hosts for URLs that should be scanned if appearing on the webpage.
 pub const SCAN_WEBPAGE_URL_HOSTS: [&str; 4] = ["bit.ly", "t.cn", "t.co", "tinyurl.com"];
-
-/// A list of illegal text in images.
-#[cfg(feature = "ocr")]
-pub const ILLEGAL_IMAGE_TEXT: [&str; 3] = [
-    "EVENT ENDS AT MIDNIGHT TODAY",
-    "First 5000 Participants Bonus",
-    "Catherine Coley",
-];
-
-/// Directory containing all illegal images.
-pub const ILLEGAL_IMAGES_DIR: &str = "./res/illegal/";
 
 /// The maximum file size in bytes of files to check for legality.
 pub const MAX_FILE_SIZE: i64 = 100 * 1024 * 1024;
 
 /// The maximum file size in bytes of images to check for legality.
 pub const IMAGE_MAX_FILE_SIZE: i64 = 20 * 1024 * 1024;
-
-/// Images are illegal when their similarity to any template image is `<= threhold`.
-pub const IMAGE_BAN_THRESHOLD: f64 = 0.5;
 
 /// The minimum number of pixels each image side must have.
 ///
